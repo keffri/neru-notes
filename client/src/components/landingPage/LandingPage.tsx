7de2bd91b99e500308b4bc6ccf33c5501921f5d7
keffri <keffri.w.neal@gmail.com>
2024-01-30 21:09:39 -0800

    basic nav and lp components

---
+++ client/src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const LandingPage: FC = () => {
+  return (
+    <section className="landing">
+      <h1 className="landing__title">Neru Notes</h1>
+    </section>
+  );
+};
+
+export default LandingPage;
