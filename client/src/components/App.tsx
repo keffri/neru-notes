90dcef8962c45ff21df95cd4926b53a68a269531
keffri <keffri.w.neal@gmail.com>
2024-02-04 04:27:26 -0800

    modal state added

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,14 +1,24 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './navigation/Navigation';
 import LandingPage from './landingPage/LandingPage';
 import Features from './features/Features';
 
 const App: FC = () => {
+  const [showAuthModal, setShowAuthModal] = useState(false);
+
+  const openAuthModal = () => {
+    setShowAuthModal(true);
+  };
+
+  const closeAuthModal = () => {
+    setShowAuthModal(false);
+  };
+
   return (
     <BrowserRouter>
       <div className="app">
-        <Navigation />
+        <Navigation openAuthModal={openAuthModal} />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/features" element={<Features />} />
