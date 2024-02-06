9a222b18fedf8525c3d6285167cc63439ffcaf8c
keffri <keffri.w.neal@gmail.com>
2024-02-06 11:46:18 -0800

    landing page layout

--- client/src/components/landingPage/LandingPage.tsx
+++ client/src/components/landingPage/LandingPage.tsx
@@ -1,9 +1,43 @@
 import React, { FC } from 'react';
 
-const LandingPage: FC = () => {
+import { EditNote, Search, Bedtime } from '@mui/icons-material';
+
+interface ModalProps {
+  openAuthModal: () => void;
+}
+
+const LandingPage: FC<ModalProps> = (props: ModalProps) => {
   return (
     <section className="landing">
-      <h1 className="landing__title">Neru Notes</h1>
+      <div className="landing__container">
+        <h1 className="landing__title">Neru Notes</h1>
+        <div className="landing__descriptions">
+          <ul className="landing__list">
+            <li>
+              <p>Track</p>
+              <EditNote className="landing__icon" />
+            </li>
+            <li>
+              <p>Analyze</p>
+              <Search className="landing__icon" />
+            </li>
+            <li>
+              <p>Sleep</p>
+              <Bedtime className="landing__icon" />
+            </li>
+          </ul>
+        </div>
+        <p className="landing__text landing--medium">
+          Sleep tracking made simple.
+        </p>
+        <button
+          data-testid="landingPage_button"
+          className="landing__button"
+          onClick={() => props.openAuthModal()}
+        >
+          Get started!
+        </button>
+      </div>
     </section>
   );
 };
