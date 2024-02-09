b90d1e8d201db3d82de11f833eadb3dc246724a5
keffri <keffri.w.neal@gmail.com>
2024-02-08 21:28:23 -0800

    updated main with router

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,18 @@
 require('dotenv').config();
 import express from 'express';
 
+const cors = require('cors');
+
 const port = process.env.PORT || 8000;
 
+const router = require('../api/routes');
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
