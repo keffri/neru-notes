24bd4c0add16cc541035dea01dd996f907b5a4c0
keffri <keffri.w.neal@gmail.com>
2024-02-13 21:29:52 -0800

    updated props and conditions

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,12 +1,22 @@
 import { FC, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import Navigation from './navigation/Navigation';
 import Auth from './auth/Auth';
 import LandingPage from './landingPage/LandingPage';
+import Home from './home/Home';
 import Features from './features/Features';
+
+type User = {
+  email: string;
+};
 
 const App: FC = () => {
   const [showAuthModal, setShowAuthModal] = useState(false);
+  const [user, setUser] = useState<User>({ email: '' });
+  const [cookies, setCookie, removeCookie] = useCookies(undefined);
+  const authToken: string = cookies.AuthToken;
+  const userEmail: string = cookies.Email;
 
   const openAuthModal = () => {
     setShowAuthModal(true);
@@ -16,16 +26,46 @@ const App: FC = () => {
     setShowAuthModal(false);
   };
 
+  const updateUser = (user: User) => {
+    setUserCallback(user);
+  };
+
+  const setUserCallback = (data: User) => {
+    setUser(data);
+  };
+
   return (
     <BrowserRouter>
       <div className="app">
-        <Navigation openAuthModal={openAuthModal} />
-        {showAuthModal && <Auth closeAuthModal={closeAuthModal} />}
-        <Routes>
-          <Route
-            path="/"
-            element={<LandingPage openAuthModal={openAuthModal} />}
+        <Navigation
+          openAuthModal={openAuthModal}
+          user={user}
+          updateUser={updateUser}
+          removeCookie={removeCookie}
+          authToken={authToken}
+        />
+        {showAuthModal && (
+          <Auth
+            closeAuthModal={closeAuthModal}
+            user={user}
+            updateUser={updateUser}
+            cookies={cookies}
+            setCookie={setCookie}
           />
+        )}
+        <Routes>
+          {!authToken && (
+            <Route
+              path="/"
+              element={<LandingPage openAuthModal={openAuthModal} />}
+            />
+          )}
+          {authToken && (
+            <Route
+              path="/"
+              element={<Home user={user} userEmail={userEmail} />}
+            />
+          )}
           <Route path="/features" element={<Features />} />
         </Routes>
       </div>
