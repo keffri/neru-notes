16bb6aa3299d6679f52c470130eb98b055641517
keffri <keffri.w.neal@gmail.com>
2024-02-13 21:32:12 -0800

    nav props + link conditions complete

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -2,11 +2,25 @@ import React, { FC } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+type User = {
+  email: string;
+};
+
 export interface NavProps {
   openAuthModal: () => void;
+  updateUser: (user: User) => void;
+  user: User;
+  removeCookie: (name: string) => void;
+  authToken: any;
 }
 
 const Navigation: FC<NavProps> = (props: NavProps) => {
+  const signOut = () => {
+    props.removeCookie('Email');
+    props.removeCookie('AuthToken');
+    window.location.reload();
+  };
+
   return (
     <Navbar expand="lg" className="navigation">
       <Container>
@@ -17,18 +31,43 @@ const Navigation: FC<NavProps> = (props: NavProps) => {
             <Nav.Link as={Link} to="/" className="navigation__link">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/features" className="navigation__link">
-              Features
-            </Nav.Link>
-            <Nav.Link className="navigation__link">
-              <button
-                data-testid="nav_button"
-                className="navigation__button"
-                onClick={() => props.openAuthModal()}
-              >
-                Get started!
-              </button>
-            </Nav.Link>
+            {props.authToken && (
+              <Nav.Link as={Link} to="#" className="navigation__link">
+                Entry
+              </Nav.Link>
+            )}
+            {props.authToken && (
+              <Nav.Link as={Link} to="#" className="navigation__link">
+                View
+              </Nav.Link>
+            )}
+            {props.authToken && (
+              <Nav.Link href="#" className="navigation__link">
+                <button
+                  data-testid="buttonLogout"
+                  className="navigation__button"
+                  onClick={() => signOut()}
+                >
+                  Sign Out
+                </button>
+              </Nav.Link>
+            )}
+            {!props.authToken && (
+              <Nav.Link as={Link} to="/features" className="navigation__link">
+                Features
+              </Nav.Link>
+            )}
+            {!props.authToken && (
+              <Nav.Link className="navigation__link">
+                <button
+                  data-testid="nav_button"
+                  className="navigation__button"
+                  onClick={() => props.openAuthModal()}
+                >
+                  Get started!
+                </button>
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
