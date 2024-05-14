e9d35f2c60ceb0d81d78a821835b72a33018236e
keffri <keffri.w.neal@gmail.com>
2024-05-14 06:04:16 -0700

    added fail conditions

--- client/src/components/view/View.tsx
+++ client/src/components/view/View.tsx
@@ -7,7 +7,19 @@ interface ViewProps {
 const View: FC<ViewProps> = (props: ViewProps) => {
   return (
     <section className="view">
-      <h1 className="view__title">View</h1>
+      {!props.cookies.AuthToken && (
+        <div className="view__fail">
+          <h2 className="view__fail--center">
+            Please log in if you wish to view your logs.
+          </h2>
+        </div>
+      )}
+
+      {props.cookies.AuthToken && (
+        <div className="view__pass">
+          <h1 className="view__title">View</h1>
+        </div>
+      )}
     </section>
   );
 };
